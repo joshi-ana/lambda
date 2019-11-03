18ef2c30fb9e1dd5cba2040859f5a91acb240fcc
Yoshihisa Kaino <yoppy1125yoppy@gmail.com>
2019-11-03 12:10:21 +0900

    Update hello-from-lambda.js

--- src/handlers/hello-from-lambda.js
+++ src/handlers/hello-from-lambda.js
@@ -2,12 +2,19 @@
  * A Lambda function that returns a string.
  */
 exports.helloFromLambdaHandler = async () => {
-    // If you change this message, you will need to adjust tests in hello-from-lambda.test.js
-    const message = 'Hello from Lambda!';
-
-    // All log statements are written to CloudWatch by default. For more information, see
-    // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
-    console.log(message);
-
-    return message;
+return {
+    statusCode: 200,
+    headers: {
+        "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify(
+      {
+        message: 'OK',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
 };
+
